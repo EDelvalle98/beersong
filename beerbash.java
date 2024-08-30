ac16043499a4ea9410eb148adc51105e52f4dfe9
EDelvalle98 <efrain.delvalle@hudsoncatholic.org>
2024-08-30 13:42:49 -0400

    Updated

--- beerbash.java
+++ beerbash.java
@@ -7,16 +7,18 @@ public class beerbash {
 
         while (beerNum > 0)
         {
-            if (beerNum == 1)
-            {
-                word = "bottle";
-            }
+
 
             System.out.print(beerNum + " " + "of beer on the wall");
             System.out.println(" " + beerNum + " " + word + " of beer");
             System.out.print("Take one down ");
             System.out.print("Pass it around ");
             beerNum = beerNum - 1;
+
+            if (beerNum == 1)
+            {
+                word = "bottle";
+            }
 
             if (beerNum > 0)
             {
